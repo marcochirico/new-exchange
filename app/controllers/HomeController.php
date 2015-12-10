f4766cfa866de80f788376aa1624b65332dcecf0
Marco Chirico <marco@mutado.com>
2015-12-10 17:35:55 +0100

    Email Events

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,8 +5,8 @@ class HomeController extends BaseController {
     protected $layout = 'master';
 
     public function index() {
-        $user = array('pippo' => 'pluto');
-        Event::fire('sendMail.interviewRequest', array($user));
+//        $user = array('pippo' => 'pluto');
+//        Event::fire('sendMail.interviewRequest', array($user));
 
         $data = new stdClass();
         $this->layout->content = View::make('index')->with('data', $data);
