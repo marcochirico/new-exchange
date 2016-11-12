7d1ba90e2abe8db0839d8cd41fcba7302c36fd64
Marco <Marco@MBP-di-User.homenet.telecomitalia.it>
2016-11-12 21:55:42 +0100

    Fix

--- app/views/shared/news.blade.php
+++ app/views/shared/news.blade.php
@@ -1,13 +1,13 @@
 <h4 class="title-bar news navbar"><i class="glyphicon glyphicon-file"></i>News List</h4>
 <div class="row">
     <?php
-    $news = Feed\Oracle::get('http://blogs.oracle.com/partnernews/feed/entries/rss?cat=Applications');
+    $news = array(); //Feed\Oracle::get('http://blogs.oracle.com/partnernews/feed/entries/rss?cat=Applications');
     if (count($news) > 0):
         $i = 1;
         foreach ($news as $n):
             ?>
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 news-box">
-                <div class="content <?php if($i!=4) echo 'border-separator'; ?>">
+                <div class="content <?php if ($i != 4) echo 'border-separator'; ?>">
                     <?php echo $n['title']; ?>
                 </div>
                 <a href="<?php echo $n['link']; ?>" target="_blank">Read more &nbsp;<span class=" glyphicon glyphicon-circle-arrow-right"></span></a>
